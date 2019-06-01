e6de405a48261c464735b1f8c297242337ff2f0e
Jan Beckmann <king-jan1999@hotmail.de>
2019-06-01 10:23:35 +0200

    first provider, basic structure

---
+++ src/domains.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+const zeitApiInstance = axios.create({
+    baseURL: 'https://api.zeit.co'
+})
+
+export const fetchUnverifiedDomains = async (zeitToken) => {
+    const result = await zeitApiInstance.get('/v4/domains', {
+        headers: {
+            Authorization: `Bearer ${zeitToken}`
+        }
+    });
+    const domains = result.data.domains;
+    return domains.filter(x => !x.verified);
+}
---
+++ src/providers/route53.js
@@ -0,0 +1,28 @@
+const util = require('util');
+
+const Route53 = require('nice-route53');
+
+export default async (domain, token, metadata) => {
+    const r53 = new Route53({
+        accessKeyId: metadata.AWS_ACCESS_KEY_ID,
+        secretAccessKey: metadata.AWS_SECRET_ACCESS_KEY
+    });
+
+    const zones = await util.promisify(r53.zones)();
+    const zone = zones.find(x => x.name === domain);
+    if(!zone) {
+        throw new Error(`No zone found for domain ${domain}`);
+    }
+
+    const result = await util.promisify(r53.setRecord)({
+        zoneId: zone.zoneId,
+        name: `_now.${domain}`,
+        type: 'TXT',
+        ttl: 3600, // TODO Configurable?
+        values: [token]
+    });
+
+    console.log(result);
+
+    return result;
+}
