44c3306ded2607739f82d4c10245f67799c533e4
Jan Beckmann <king-jan1999@hotmail.de>
2019-06-01 11:39:47 +0200

    temp commit

---
+++ src/providers/index.js
@@ -0,0 +1,8 @@
+const route53 = require("./route53");
+
+module.exports = {
+    route53: {
+        name: "Route 53",
+        provider: route53
+    }
+};
--- src/providers/route53.js
+++ src/providers/route53.js
@@ -2,17 +2,27 @@ const util = require("util");
 
 const Route53 = require("nice-route53");
 
-export default async (domain, token, metadata) => {
-    const r53 = new Route53({
+const getR53Instance = (metadata) => {
+    return new Route53({
         accessKeyId: metadata.AWS_ACCESS_KEY_ID,
         secretAccessKey: metadata.AWS_SECRET_ACCESS_KEY
     });
+};
 
+const getZoneForDomain = async (r53, domain) => {
     const zones = await util.promisify(r53.zones)();
     const zone = zones.find(x => x.name === domain);
-    if(!zone) {
+    if (!zone) {
         throw new Error(`No zone found for domain ${domain}`);
     }
+
+    return zone;
+};
+
+export const setVerify = async (domain, token, metadata) => {
+    const r53 = getR53Instance(metadata);
+
+    const zone = await getZoneForDomain(r53, domain);
 
     const result = await util.promisify(r53.setRecord)({
         zoneId: zone.zoneId,
@@ -20,6 +30,24 @@ export default async (domain, token, metadata) => {
         type: "TXT",
         ttl: 3600, // TODO Configurable?
         values: [token]
+    });
+
+    console.log(result);
+
+    return result;
+};
+
+export const setAlias = async (domain, metadata) => {
+    const r53 = getR53Instance(metadata);
+
+    const zone = await getZoneForDomain(r53, domain);
+
+    const result = await util.promisify(r53.setRecord)({
+        zoneId: zone.zoneId,
+        name: ``, // TODO Configurable?
+        type: "CNAME",
+        ttl: 3600, // TODO Configurable?
+        values: ["alias.zeit.co"]
     });
 
     console.log(result);
